d61ef294b29f58300261f8af68896bab360d351c
SajeebChakraborty <sajeebchakraborty.cse2000@gmail.com>
2023-04-21 04:11:14 +0600

    method declare & call

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 var app=new Vue({
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        welcome: 'Welcome to Vue.js',
     },
     methods: {
         myOutcome() {
             return "good";
         },
         myAge() {
-            return '${this.message} . js';
+            return '${this.welcome} . good';
         }
     }
 })
